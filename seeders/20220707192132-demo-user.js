c89494987f4f15935042ebb0908e7830a31992e6
danilojezernik <dani.jezernik@gmail.com>
2022-07-07 21:33:06 +0200

    added files

---
+++ seeders/20220707192132-demo-user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
